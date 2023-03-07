7fff86c533f10c57b1562773a8cf9b3e3513db65
new name <truongquangkhanh1992@gmail.com>
2023-03-07 21:57:00 +0700

    tao bang menu

--- resources/views/partials/siderbar.blade.php
+++ resources/views/partials/siderbar.blade.php
@@ -23,8 +23,7 @@
         <!-- SidebarSearch Form -->
         <div class="form-inline">
             <div class="input-group" data-widget="sidebar-search">
-                <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                    aria-label="Search">
+                <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
                 <div class="input-group-append">
                     <button class="btn btn-sidebar">
                         <i class="fas fa-search fa-fw"></i>
@@ -35,8 +34,7 @@
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
-            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-                data-accordion="false">
+            <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
                 <!-- Add icons to the links using the .nav-icon class
               with font-awesome or any other icon font library -->
                 <li class="nav-item">
@@ -45,6 +43,14 @@
                         <p>
                             Danh muc san pham
                             <span class="right badge badge-danger">New</span>
+                        </p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{ route('menus.index') }}" class="nav-link">
+                        <i class="nav-icon fas fa-th"></i>
+                        <p>
+                            Menus
                         </p>
                     </a>
                 </li>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MenuController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 
@@ -30,4 +31,10 @@ Route::group(['prefix' => 'categories', 'as' => 'categories.'], function () {
     Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('edit');
     Route::post('update/{id}', [CategoryController::class, 'update'])->name('update');
     Route::get('delete/{id}', [CategoryController::class, 'delete'])->name('delete');
+});
+
+Route::group(['prefix' => 'menus', 'as' => 'menus.'], function () {
+    Route::get('', [MenuController::class, 'index'])->name('index');
+    Route::get('create', [MenuController::class, 'create'])->name('create');
+    Route::post('store', [MenuController::class, 'store'])->name('store');
 });
